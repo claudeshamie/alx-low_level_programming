b7a06c6e4d6bdfed15738847d0621e523349f6a8
claudeshamie <claudeshamie@gmail.com>
2022-09-08 14:44:00 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-*   main -print the string in the put function
+* main -print the string in the put function
 * 
-*   Description: using main fuction
-*   this program prints "Programming is like building a multilingual puzzle"
-*   Return: 0
+* Description: using main fuction
+* this program prints "Programming is like building a multilingual puzzle"
+* Return: 0
 */ 
 int main(void)
 {	
