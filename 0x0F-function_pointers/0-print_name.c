f4fc27f48e8cac6ab032e2a8ea501136fd477420
claudeshamie <claudeshamie@gmail.com>
2022-10-12 09:59:53 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "FUNCTION_POINTERS_H_"
+
+/**
+* print_name - prints a name
+* @name: name to print
+* @f: function to print with
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
